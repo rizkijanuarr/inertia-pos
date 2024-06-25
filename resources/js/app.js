7cc8a5c56781f7f2f153219937432e6fe3dacde7
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2024-06-25 23:48:09 +0700

    feat(inertia): konfigurasi inertia with mixins

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,23 @@ createInertiaApp({
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
+            //set mixins
+            .mixin({
+                methods: {
+                    //method "hasAnyPermission"
+                    hasAnyPermission: function (permissions) {
+                        //get permissions from props
+                        let allPermissions = this.$page.props.auth.permissions;
+
+                        let hasPermission = false;
+                        permissions.forEach(function (item) {
+                            if (allPermissions[item]) hasPermission = true;
+                        });
+
+                        return hasPermission;
+                    },
+                },
+            })
             .use(plugin)
             .mount(el);
     },
