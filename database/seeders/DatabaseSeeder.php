364e850def95231394de3829ce95e28efd2228dd
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2024-06-25 23:27:44 +0700

    [seeder]:[permissions]: added permissions seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RolesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
     }
 }
