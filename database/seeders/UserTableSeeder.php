18f2ed90e899977a23301887c900048b13193a52
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2024-06-25 23:37:43 +0700

    add📦[inertia]:[add + setup inertia server side]

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserTableSeeder extends Seeder
 {
